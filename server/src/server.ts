3cca8137d50e8bfbf111c8c37457cde810219c01
g-adler <guiadler@hotmail.com>
2020-08-04 11:26:49 -0300

    setting up DB connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log('ZA WARUUUDOOOO');
+
+import express from 'express';
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req,res) =>{
+    return res.json({
+          message : 'oi,wot ya fookin want?'
+        });
+})
+
+app.listen(3333);
