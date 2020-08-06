a85786a04b1d0b4d9b9ce4af21a8192e7f2e29a5
g-adler <guiadler@hotmail.com>
2020-08-05 21:04:24 -0300

    wrappig up teacher form

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
@@ -6,8 +6,22 @@ import './styles.css';
 import Input from '../../components/Input';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/TextArea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day:0,from: '',to: ''}
+  ])
+
+  function addNewScheduleItem () {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day:0,from: '',to: ''}
+
+    ]);
+  }
+
   return (
     <div id='page-teacher-form' className='container ' >
       <PageHeader
@@ -22,25 +36,55 @@ function TeacherForm() {
           <Input name='name' label='Nome completo' />
           <Input name='avatar' label='Avatar' />
           <Input name='whatsapp' label='Whatsapp' />
-
+          <Textarea name='bio' label='Biografia' />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a Aula</legend>
 
-          <Input name='subject' label='Matéria' />
+          <Select 
+            name='subject'
+            label='Matéria'
+            options={ [ 
+              { value: 'Artes', label : 'Artes' },
+              { value: 'Biologia', label : 'Biologia' },
+              { value: 'Matemática', label : 'Matemática' },
+              { value: 'Química', label : 'Química' },
+              { value: 'Socialismo como um valor absoluto de paz entre as sociedades', label : 'Socialismo como um valor absoluto de paz entre as sociedades' }
+             ] }
+          />
           <Input name='cost' label='Custo da sua Hora/aula' />
 
         </fieldset>
 
         <fieldset>
-          <legend>Seus Dados</legend>
-
-          <Input name='name' label='Nome completo' />
-          <Input name='avatar' label='Avatar' />
-          <Input name='whatsapp' label='Whatsapp' />
-
+          <legend>Horários Disponíveis
+            <button onClick={addNewScheduleItem} type='button'> + Novo horário
+            </button>
+          </legend>
+          {scheduleItems.map(scheduleItem => {
+            return(
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select 
+                  name='week_day'
+                  label='Dia da semana'
+                  options={ [ 
+                    { value: '1', label : 'Segunda' },
+                    { value: '2', label : 'Terça' },
+                    { value: '3', label : 'Quarta' },
+                    { value: '4', label : 'Quinta' },
+                    { value: '5', label : 'Sexta' },
+                    { value: '6', label : 'Sábado' },
+                    { value: '0', label : 'Não' }
+                    ] }
+                />
+                <Input name='from' label='Das' type='time'/>
+                <Input name='to' label='Até' type='time'/>  
+            </div> 
+            )}
+            )}
         </fieldset>
+
         <footer>
           <p>
             <img src={warningIcon} alt='Aviso imporante' />
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -6,16 +6,37 @@ import './styles.css'
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem'
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList() {
     return(
         <div id="page-teacher-list" className="container">
            <PageHeader title="Estes são os proffys disponiveis" >
             <form id="search-teachers">
-                
-                <Input name='subject' label='Matéria' />
+        
+            <Select 
+            name='subject'
+            label='Matéria'
+            options={ [ 
+                { value: 'Artes', label : 'Artes' },
+                { value: 'Biologia', label : 'Biologia' },
+                { value: 'Matemática', label : 'Matemática' },
+                { value: 'Química', label : 'Química' },
+                { value: 'Socialismo como um valor absoluto de paz entre as sociedades', label : 'Socialismo como um valor absoluto de paz entre as sociedades' }
+            ] } />
 
-                <Input name='week_day' label='Dia da semana' />
+            <Select 
+                name='week_day'
+                label='Dia da semana'
+                options={ [ 
+                    { value: '1', label : 'Segunda' },
+                    { value: '2', label : 'Terça' },
+                    { value: '3', label : 'Quarta' },
+                    { value: '4', label : 'Quinta' },
+                    { value: '5', label : 'Sexta' },
+                    { value: '6', label : 'Sábado' },
+                    { value: '0', label : 'Não' }
+                ] } />
 
                 <Input name='time' type='time' label='Hora' />
 
